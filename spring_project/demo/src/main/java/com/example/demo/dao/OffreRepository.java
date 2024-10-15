c75a2883c4b0bbc232668d1104a85a88507d6899
Akram Zaabi <122676637+AkramZaabi@users.noreply.github.com>
2024-10-15 20:36:07 +0100

    Delete spring_project/demo directory

--- spring_project/demo/src/main/java/com/example/demo/dao/OffreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entities.Offre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OffreRepository extends JpaRepository<Offre,Long> {
-}
